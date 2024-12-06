b743cc6724529412111e1ad3fee0e1f2e0fd1d55
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-12-05 21:26:40 -0500

    Added files

---
+++ MoonShot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Jason Cox on 12/1/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
+
